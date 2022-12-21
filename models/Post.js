869e451e1b523cd9e32b96f3fbb27a98974668c8
saitek <saitek300@gmail.com>
2022-12-21 12:15:35 -0800

    created post model

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Post.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    body:{
+        type: DataTypes.STRING,
+        allowNull: false,
+
+    },
+    postedBy:{
+        type: DataTypes.INTEGER,
+        references:{
+            model: 'user',
+            key: 'id',
+        },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
