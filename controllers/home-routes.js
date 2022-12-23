0c01447c4773701c34e0dc5ef92aac678ec43d02
saitek <saitek300@gmail.com>
2022-12-22 17:19:24 -0800

    re-imagined home routes to include postData

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,33 +1,37 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
 
 router.get('/', async (req, res) => {
   try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
     });
+    const posts = postData.map((post) => post.get({plain:true}));
 
-    // Serialize user data so templates can read it
-    const users = userData.map((post) => post.get({ plain: true }));
-
-    // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/dashboard', (req, res) => {
-  if ( !req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
+router.get('/dashboard',withAuth, (req, res) => {
+  
+    res.render('dashboard');
+  
 });
 
 router.get('/login', (req, res) => {
-  // If a session exists, redirect the request to the homepage
+ 
   if (req.session.logged_in) {
     res.redirect('/');
     return;
