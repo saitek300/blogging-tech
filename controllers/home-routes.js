d26d7669939cbcb419601d6b3e824c8b1a5e7a7d
saitek <saitek300@gmail.com>
2023-01-01 09:24:33 -0800

    requiring log in to view dashboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,25 +13,35 @@ router.get('/', async (req, res) => {
         },
       ],
     });
-    const posts = postData.map((post) => post.get({plain:true}));
 
-    res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in
-    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render(
+      'homepage', 
+      {
+        posts,
+        logged_in: req.session.logged_in
+      }
+    );
+
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/dashboard',withAuth, (req, res) => {
-  
-    res.render('dashboard');
-  
+router.get('/dashboard', withAuth, (req, res) => {
+
+  res.render(
+    'dashboard', 
+    {
+      logged_in: req.session.logged_in
+    }
+  )
+
 });
 
 router.get('/login', (req, res) => {
- 
+
   if (req.session.logged_in) {
     res.redirect('/');
     return;
